39bf5204ef5a2d0550559dc9e5bc40dc606bfefa
Mario Ancic <mancic12@gmail.com>
2024-07-17 21:31:41 +0200

    Add files via upload

--- Python3 Tkinter/Balloon freelift calculator.py
+++ Python3 Tkinter/Balloon freelift calculator.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        Balloon lift calculator
 # Purpose:
 #
 # Author:      9A4AM
@@ -21,33 +21,33 @@ def izracunaj_uzgon():
 
         uzgon_na_vagi = (masa_sonde + masa_konopca + zeljeni_uzgon) - masa_adaptera
 
-        messagebox.showinfo("Rezultat", f"Uzgon na vagi: {uzgon_na_vagi} grama")
+        messagebox.showinfo("Result", f"Lift on scale: {uzgon_na_vagi} gram")
     except ValueError:
-        messagebox.showerror("Greška", "Molimo unesite validne numeričke vrednosti")
+        messagebox.showerror("Error", "Please enter nummeric value")
 
 # Kreiraj glavni prozor
 root = tk.Tk()
-root.title("Izračunavanje uzgona za meteo balone")
+root.title("Balloon lift calculator")
 
-# Dodaj labele i unosna polja
-tk.Label(root, text="Masa adaptera (g):").grid(row=0, column=0, padx=10, pady=5)
+# Dodaj labele i polja za unos
+tk.Label(root, text="Weight adapter (g):").grid(row=0, column=0, padx=10, pady=5)
 entry_masa_adaptera = tk.Entry(root)
 entry_masa_adaptera.grid(row=0, column=1, padx=10, pady=5)
 
-tk.Label(root, text="Masa konopca (g):").grid(row=1, column=0, padx=10, pady=5)
+tk.Label(root, text="Weight rope (g):").grid(row=1, column=0, padx=10, pady=5)
 entry_masa_konopca = tk.Entry(root)
 entry_masa_konopca.grid(row=1, column=1, padx=10, pady=5)
 
-tk.Label(root, text="Masa sonde (g):").grid(row=2, column=0, padx=10, pady=5)
+tk.Label(root, text="Weight sonde (g):").grid(row=2, column=0, padx=10, pady=5)
 entry_masa_sonde = tk.Entry(root)
 entry_masa_sonde.grid(row=2, column=1, padx=10, pady=5)
 
-tk.Label(root, text="Željeni uzgon (g):").grid(row=3, column=0, padx=10, pady=5)
+tk.Label(root, text="Target lift (g):").grid(row=3, column=0, padx=10, pady=5)
 entry_zeljeni_uzgon = tk.Entry(root)
 entry_zeljeni_uzgon.grid(row=3, column=1, padx=10, pady=5)
 
-# Dodaj dugme za izračunavanje
-tk.Button(root, text="Izračunaj uzgon", command=izracunaj_uzgon).grid(row=4, columnspan=2, pady=10)
+# Dodaj gumb za izračunavanje
+tk.Button(root, text="CALCULATE LIFT WITH ADAPTER", command=izracunaj_uzgon).grid(row=4, columnspan=2, pady=10)
 
-# Pokreni glavni loop
+# Pokreni glavnu petlju
 root.mainloop()
